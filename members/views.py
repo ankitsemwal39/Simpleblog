c2881fa0f4b3dd8560aae1a79067394a0d5b0980
ankit semwal <ankitsemwal39@gmail.com>
2021-04-12 13:55:55 +0530

    initial commit

---
+++ members/views.py
@@ -0,0 +1,11 @@
+
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from .forms import SignupForm
+
+
+class UserRegisterView(generic.CreateView):
+    form_class = SignupForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
