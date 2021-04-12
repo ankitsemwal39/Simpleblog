976fc07d74014c324913758aed8a071e4cbf5416
ankit semwal <ankitsemwal39@gmail.com>
2021-04-12 22:27:34 +0530

    app for

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 
+
+from pathlib import Path
 import django_heroku
 import dj_database_url
 from decouple import config
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,4 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 django_heroku.settings(locals())
+
